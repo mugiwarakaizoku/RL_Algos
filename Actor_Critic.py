06e5188007f2ec8ff92606ddbc59567c64790000
mugiwarakaizoku <47596212+mugiwarakaizoku@users.noreply.github.com>
2022-03-14 20:32:18 +0530

    formatting_changes

--- Actor_Critic.py
+++ Actor_Critic.py
@@ -44,19 +44,14 @@ class Actor_Critic_Network(keras.Model):
         self.fc2 = Dense(32,activation='relu')
         self.v = Dense(1,activation='linear')
         self.pi = Dense(total_actions,activation='softmax')
+        
     def call(self,state):
-        #print('state: {}'.format(state))
         state_tensor = tf.convert_to_tensor([state])
-        #print('state_tensor:{} and len_state_tensor:{}'.format(state_tensor,len(state_tensor)))
         value = self.fc1(state_tensor)
         value = self.fc2(value)
         value_fn = self.v(value)[0]
-        #print('state:{} and value_fn:{}'.format(state,value_fn))
         policy_fn = self.pi(value)
         policy_fn = policy_fn[0]
-        #print('value_fn:{} and length_of_value_fn:{}'.format(value_fn,len(value_fn)))
-        #print('state:{} and state_length:{}'.format(state,len(state)))
-        #print('policy_fn:{} and length_of_policy_fn:{}'.format(policy_fn,len(policy_fn)))
         return value_fn,policy_fn
 
 class Agent():
@@ -67,26 +62,21 @@ class Agent():
     
     def add_transition(self,transition):
       self.data.append(transition)
-      #print('length of self.data at add_transition: {} and self.data: {}'.format(len(self.data),self.data))
 
     def make_batch(self):
       state_list,action_list,reward_list,next_state_list,done_list = [],[],[],[],[]
       for transition in self.data:
-        #print('length of transition at make_batch: {} and transition: {}'.format(len(transition),transition))
         state_list.append(transition[0])
         action_list.append(transition[1])
         reward_list.append(transition[2])
         next_state_list.append(transition[3])
         done_list.append(transition[4])
       self.data=[]
-      #print('length of state_list at make_batch: {} and state_list: {}'.format(len(state_list),state_list))
       return np.array(state_list),action_list,reward_list,np.array(next_state_list),done_list
 
     def chose_action(self,state):
-        #print('state at chose_action: {}'.format(state))
         _,action_probs = self.actor_critic.call(state)
         action_prob_dist = tfp.distributions.Categorical(probs = action_probs)
-        # print('action_prob_dist: {}'.format(action_prob_dist))
         action = action_prob_dist.sample()
         action = action.numpy()
         print(action)
@@ -96,33 +86,17 @@ class Agent():
         state,action,reward,next_state,done = self.make_batch()
         with tf.GradientTape() as tape:
           v_curr_state,pi_curr_state = self.actor_critic.call(state)
-          #print('shape is: {}  and  state: {}'.format(tf.shape(state),state))
-          #print('shape is: {}  and  v_curr_state before squeezing: {}'.format(tf.shape(v_curr_state),v_curr_state))
           v_next_state,_ = self.actor_critic.call(next_state)
-          #print('state at v_next_state: {}'.format(state))
           v_curr_state = tf.squeeze(v_curr_state)
-          #print('shape is: {} and  v_curr_state after squeezing: {}'.format(tf.shape(v_curr_state),v_curr_state))
           v_next_state = tf.squeeze(v_next_state)
-          #print('v_curr_state: {} and curr_state_length:{}'.format(v_curr_state,len(v_curr_state)))
-          #print('v_next_state: {} and next_state_length:{}'.format(v_next_state,len(v_next_state)))
           delta = reward[0]+v_next_state*GAMMA*(1-int(done[0]))-v_curr_state
-          #print('reward: {} len_reward: {} v_next_state: {} v_curr_state: {}'.format(reward,len(reward),v_next_state,v_curr_state))
-          #print('delta_length: {} delta: {}'.format(len(delta),delta))
-          #print(type(delta))
-          #print('pi_curr_state: {}'.format(pi_curr_state))
           curr_action_dist = tfp.distributions.Categorical(probs = pi_curr_state)
-          #print('curr_action_dist: {}'.format(curr_action_dist))
           log_probs = curr_action_dist.log_prob(action)
-          #print('log_probs: {}'.format(log_probs))
 
           actor_loss = -log_probs*delta
           critic_loss = delta**2
-          #print('actor_loss: {}  critic_loss: {}'.format(actor_loss,critic_loss))
-          #print('actor_loss_length: {} critic_loss_length: {}'.format(len(actor_loss),len(critic_loss)))
           total_loss = actor_loss + critic_loss
           total_loss = tf.reduce_sum(total_loss)
-          #print(total_loss)
-          #print(self.actor_critic.trainable_variables)
           gradient = tape.gradient(total_loss,self.actor_critic.trainable_variables)
           self.actor_critic.optimizer.apply_gradients(zip(gradient,self.actor_critic.trainable_variables))
         return total_loss
